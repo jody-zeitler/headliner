2445c9c249cc03bd6122efe460f271ac2234df48
Jody Zeitler <jody.zeitler@gmail.com>
2017-07-02 18:09:48 -0500

    Extend the base app class with a freeform mode

---
+++ headliner.js
@@ -0,0 +1,147 @@
+const {generateNewsTitles} = require('./wikinews');
+
+const KEY_C_C = 3;
+const KEY_BACKSPACE = 8;
+
+const ASCII_MIN = 32; // space
+const ASCII_MAX = 126; // tilde
+
+class Headliner {
+	constructor({enableErrorBell = true} = {}) {
+		this.processStdin = this.processStdin.bind(this);
+		this.newsTitles = generateNewsTitles();
+
+		this.enableErrorBell = enableErrorBell;
+
+		this.currentPhrase = null;
+		this.phraseIndex = 0;
+
+		this.correctCharacters = 0;
+		this.missedCharacters = 0;
+
+		this.startTime = 0;
+		this.totalTime = 0;
+	}
+
+	setupStdin() {
+		process.stdin.setRawMode(true);
+		process.stdin.on('data', this.processStdin);
+	}
+
+	async newTitle() {
+		this.currentPhrase = null;
+		this.phraseIndex = 0;
+		this.currentPhrase = await this.newsTitles.next().value;
+		console.log(this.currentPhrase);
+	}
+
+	processStdin(data) {
+		const code = data.readInt8();
+		if (code === KEY_C_C) {
+			process.exit(0);
+			return;
+		}
+		if (!this.currentPhrase) {
+			return;
+		}
+		this.startTime = this.startTime || Date.now();
+		const currentCharCode = this.currentPhrase.charCodeAt(this.phraseIndex);
+		if (code === currentCharCode || currentCharCode >= ASCII_MAX) {
+			this.recordCorrectCharacter();
+		} else {
+			this.missedCharacters++;
+			if (this.enableErrorBell === true) {
+				soundBell();
+			}
+		}
+	}
+
+	recordCorrectCharacter() {
+		process.stdout.write(this.currentPhrase[this.phraseIndex]);
+		this.correctCharacters++;
+		if (++this.phraseIndex >= this.currentPhrase.length) {
+			process.stdout.write('\n');
+			this.endTitle();
+			this.newTitle();
+		}
+	}
+
+	endTitle() {
+		this.currentPhrase = null;
+		this.totalTime += Date.now() - this.startTime;
+		this.startTime = null;
+		const words = this.correctCharacters / 5;
+		const minutes = this.totalTime / 1000 / 60;
+		const wpm = (words / minutes).toFixed(1);
+		const characters = this.correctCharacters + this.missedCharacters;
+		const accuracy = ((this.correctCharacters / characters) * 100).toFixed(1);
+		console.log(`WPM: ${wpm}. Accuracy: ${accuracy}%`);
+	}
+}
+
+class FreeformHeadliner extends Headliner {
+	constructor() {
+		super(...arguments);
+		this.missDebt = 0;
+	}
+
+	async newTitle() {
+		this.missDebt = 0;
+		super.newTitle();
+	}
+
+	processStdin(data) {
+		const code = data.readInt8();
+		if (code === KEY_C_C) {
+			process.exit(0);
+			return;
+		}
+		if (!this.currentPhrase) {
+			return;
+		}
+		this.startTime = this.startTime || Date.now();
+		const currentCharCode = this.currentPhrase.charCodeAt(this.phraseIndex);
+		switch (code) {
+			case currentCharCode:
+				if (this.missDebt === 0) {
+					this.recordCorrectCharacter();
+				}
+				return;
+			case KEY_BACKSPACE:
+				if (this.missDebt > 0) {
+					this.missDebt--;
+					writeBackspace();
+				} else if (this.phraseIndex > 0) {
+					this.phraseIndex--;
+					writeBackspace();
+				}
+				return;
+			default:
+				if (currentCharCode > ASCII_MAX && this.missDebt === 0) {
+					this.recordCorrectCharacter(); // e for effort
+				} else {
+					this.missedCharacters++;
+					if (this.enableErrorBell === true) {
+						soundBell();
+					}
+					if (code >= ASCII_MIN) {
+						this.missDebt++;
+						process.stdout.write(data);
+					}
+				}
+		}
+	}
+}
+
+function writeBackspace() {
+	process.stdout.write('\b \b');
+}
+
+function soundBell() {
+	process.stderr.write('\u0007');
+}
+
+module.exports = {
+	Headliner,
+	FreeformHeadliner
+};
--- index.js
+++ index.js
@@ -1,71 +1,21 @@
-const {generateNewsTitles} = require('./wikinews');
+const {Headliner, FreeformHeadliner} = require('./headliner');
 
-const KEY_C_C = 3;
-const KEY_BACKSPACE = 8;
-
-class Headliner {
-	constructor() {
-		this.newsTitles = generateNewsTitles();
-
-		this.currentPhrase = null;
-		this.phraseIndex = 0;
-
-		this.correctCharacters = 0;
-		this.missedCharacters = 0;
-
-		this.startTime = 0;
-		this.totalTime = 0;
-	}
-
-	setupStdin() {
-		process.stdin.setRawMode(true);
-		process.stdin.on('data', (data) => {
-			const code = data.readInt8();
-			if (code === KEY_C_C) {
-				process.exit(0);
-				return;
-			}
-			if (!this.currentPhrase) {
-				return;
-			}
-			this.startTime = this.startTime || Date.now();
-			const currentCharCode = this.currentPhrase.charCodeAt(this.phraseIndex);
-			if (code === currentCharCode || currentCharCode > 127) {
-				process.stdout.write(this.currentPhrase[this.phraseIndex]);
-				this.correctCharacters++;
-				if (++this.phraseIndex >= this.currentPhrase.length) {
-					process.stdout.write('\n');
-					this.endTitle();
-					this.newTitle();
-				}
-			} else {
-				this.missedCharacters++;
-			}
-		});
-	}
-
-	endTitle() {
-		this.currentPhrase = null;
-		this.totalTime += Date.now() - this.startTime;
-		this.startTime = null;
-		const words = this.correctCharacters / 5;
-		const minutes = this.totalTime / 1000 / 60;
-		const wpm = (words / minutes).toFixed(1);
-		const characters = this.correctCharacters + this.missedCharacters;
-		const accuracy = ((this.correctCharacters / characters) * 100).toFixed(1);
-		console.log(`WPM: ${wpm}. Accuracy: ${accuracy}%`);
-	}
-
-	async newTitle() {
-		this.currentPhrase = null;
-		this.phraseIndex = 0;
-		this.currentPhrase = await this.newsTitles.next().value;
-		console.log(this.currentPhrase);
-	}
-}
+/**
+ * The base mode only echos characters that are correct.
+ * Freeform mode makes you do your own error correction,
+ * just like in real life!
+ */
+const MODE = 'freeform';
+/**
+ * Sends a bell character to stderr whenever a mistake
+ * is made. Turn up those speakers for maximum negative
+ * reinforcement.
+ */
+const BELL = true;
 
 async function main() {
-	const app = new Headliner();
+	const HeadlinerMode = MODE === 'freeform' ? FreeformHeadliner : Headliner;
+	const app = new HeadlinerMode({enableErrorBell: BELL});
 	app.setupStdin();
 	app.newTitle();
 }
